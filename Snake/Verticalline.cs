470673e71a579333dca0bde9b341e0642279590c
Dmitriy <the.wale@yandex.ru>
2020-04-22 21:59:28 +0300

    add Horizontal line

---
+++ Snake/Verticalline.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Verticalline
+    {
+        List<Point> plist;
+
+        public Verticalline(int yTop, int yButtom, int x, char sym)
+        {
+            plist = new List<Point>();
+            for (int y = yTop; y <= yButtom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
